f1f2ae27db0ac0819bc2c8afd96396d8d40e9c4c
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-01-01 19:14:48 +0400

    back to site for the server api base

--- netflix-ui/src/utils/configs.js
+++ netflix-ui/src/utils/configs.js
@@ -2,6 +2,6 @@ export const API_KEY = "6cec1c7011f7dcd684510c8c1c33cd16";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 
 // for local development 
-export const Server_API_Base = "http://localhost:4000";
+//export const Server_API_Base = "http://localhost:4000";
 
-//export const Server_API_Base = "https://fakeflix-backend-api-tgtdrwmp3q-nw.a.run.app";
+export const Server_API_Base = "https://fakeflix-backend-api-tgtdrwmp3q-nw.a.run.app";
